6c17c6c8fa8b1cd6de65eda937b02eafe98dc557
Réhan <58563932+RehanKn@users.noreply.github.com>
2020-10-24 09:39:42 +0200

    Update segmentation.c

--- segmentation.c
+++ segmentation.c
@@ -182,6 +182,16 @@ void charCut(SDL_Surface *img)
 }
 
 
+SDL_Surface* increaseChar(SDL_Surface *img)
+{
+  SDL_Surface *dest = SDL_CreateRGBSurface(SDL_HWSURFACE,
+                        28,
+                        28,
+                        img->format->BitsPerPixel,0,0,0,0);
+  SDL_SoftStretch(img, NULL, dest, NULL);
+  return dest;
+}
+
 void isolateChar(SDL_Surface *img, struct Neural_Network *net)
 {
   //Variables
