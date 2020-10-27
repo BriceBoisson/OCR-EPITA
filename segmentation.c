b5eeeef630a675451c2185cdb3233e948e8ef7e2
Réhan <58563932+RehanKn@users.noreply.github.com>
2020-10-27 15:41:01 +0100

    Update segmentation.c

--- segmentation.c
+++ segmentation.c
@@ -178,6 +178,7 @@ void DrawAColumn(SDL_Surface *img,int y, int x, int end_line)
 /*  Blocks cutting  */
 
 
+// renvoie la moyenne d'espacement entre les pixels noir pour avoir une idee de l'espacement des blocs
 int pixelSpacingHorizontal(SDL_Surface *img)
 {
   Uint32 pixel;
@@ -192,17 +193,18 @@ int pixelSpacingHorizontal(SDL_Surface *img)
     for(int j = 0; j < img -> w; j++)
     {
       pixel = getpixel(img, j, i);
-      SDL_GetRGB(pixel, img -> format, &r, &g, &b);
-      if(r == 255 && g == 255 && b == 255)
+      SDL_GetRGB(pixel, img -> format, &r, &g, &b);    // recup les r g b du pixel en cours (j, i) car on est en horizontal
+      if(r == 255 && g == 255 && b == 255)    // incremente white ou black selon qu'il est noir ou blanc
       {white++;}
       else
       {black++;}
     }
 
   }
-  return (white / (black / 2));
+  return (white / (black / 2));    // renvoie la moyenne ( nombre de blanc / (nombre de noirs / 2)  )
 }
 
+// fonctionne pareil que la version horizontale
 int pixelSpacingVertical(SDL_Surface *img)
 {
   Uint32 pixel;
@@ -217,26 +219,27 @@ int pixelSpacingVertical(SDL_Surface *img)
     for(int j = 0; j < img -> h; j++)
     {
       pixel = getpixel(img, i, j);
-      SDL_GetRGB(pixel, img -> format, &r, &g, &b);
-      if(r == 255 && g == 255 && b == 255)
+      SDL_GetRGB(pixel, img -> format, &r, &g, &b);    // recup les r g b du pixel (i, j) car vertical
+      if(r == 255 && g == 255 && b == 255)    // pareil qu'en version horizontal
       {white++;}
       else
       {black++;}
     }
   }
-  return(white / (black / 2));
+  return(white / (black / 2));    //pareil qu'en horizontal
 }
 
 
 /*  RLSA algorithm  */
 
+// remplis les espaces horizontaux entre deux blocs en noir
 void blockDetection_horizontal(SDL_Surface *img)
 { 
   Uint32 pixel;
   Uint8 r;
   Uint8 g;
   Uint8 b;
-  int hori = pixelSpacingHorizontal(img)*4;
+  int hori = pixelSpacingHorizontal(img)*4;    // c'est une moyenne de l'espacement, pour avoir un seuil adapté a l'image et pas reunir deux blocs entre eux par erreur
   int countWhite;
 
   for(int i = 0; i < img -> h; i++)
@@ -245,14 +248,14 @@ void blockDetection_horizontal(SDL_Surface *img)
     for(int j = 0; j < img -> w; j++)
     {
       pixel = getpixel(img, j, i);
-      SDL_GetRGB(pixel, img -> format, &r, &g, &b);
-      if(r == 255 && g == 255 && b == 255)
+      SDL_GetRGB(pixel, img -> format, &r, &g, &b);    // on recup le rgb de (j, i) car horitzontal
+      if(r == 255 && g == 255 && b == 255)    // on ajoute un blanc au compteur si le pixel est blanc
       {
         countWhite++;
       }
-      if(r == 0 && g == 0 && b == 0)
+      if(r == 0 && g == 0 && b == 0)    // sinon on compare le nombre de pixels blancs entre les deux pixels noirs avec hori
       {
-        if(countWhite <= hori)
+        if(countWhite <= hori)    // si c'est inferieur on rempli l'espace de noir pour creer le bloc
         {
             int k = j - 1;
             while(countWhite > 0)
@@ -263,7 +266,7 @@ void blockDetection_horizontal(SDL_Surface *img)
               k--;
             }
         }
-        else
+        else    // sinon on a deux blocs separes donc il ne faut pas remplir l'espace
         {
           countWhite = 0;
         }
@@ -271,6 +274,8 @@ void blockDetection_horizontal(SDL_Surface *img)
     }
   }
 }
+
+// remplis les espaces verticaux entre deux blocs. fonctionne pareil que la version horizontale
 void blockDetection_vertical(SDL_Surface *img)
 {
   Uint32 pixel;
