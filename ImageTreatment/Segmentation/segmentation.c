3134656d7e7c4b997cdcaadb4017c285b23ea88d
pierre jacobe <pierre.jacobe@epita.fr>
2020-10-28 17:37:32 +0100

    add

--- ImageTreatment/Segmentation/segmentation.c
+++ ImageTreatment/Segmentation/segmentation.c
@@ -21,7 +21,7 @@ int FindBlackPixel(SDL_Surface *img, int x)
         bool = 0;
         break;
     }
-    
+
  }
 
  return bool;
@@ -172,17 +172,18 @@ int pixelSpacingHorizontal(SDL_Surface *img)
     for(int j = 0; j < img -> w; j++)
     {
       pixel = getpixel(img, j, i);
-      SDL_GetRGB(pixel, img -> format, &r, &g, &b);
-      if(r == 255 && g == 255 && b == 255)
+      SDL_GetRGB(pixel, img -> format, &r, &g, &b);    // recup les r g b du pixel en cours (j, i) car on est en horizontal
+      if(r == 255 && g == 255 && b == 255)    // incremente white ou black selon qu'il est noir ou blanc
       {white++;}
       else
       {black++;}
     }
 
   }
-  return (white / (black));
+  return (white) / (black/2);    // renvoie la moyenne ( nombre de blanc / (nombre de noirs / 2)  )
 }
 
+// fonctionne pareil que la version horizontale
 int pixelSpacingVertical(SDL_Surface *img)
 {
   Uint32 pixel;
@@ -197,18 +198,15 @@ int pixelSpacingVertical(SDL_Surface *img)
     for(int j = 0; j < img -> h; j++)
     {
       pixel = getpixel(img, i, j);
-      SDL_GetRGB(pixel, img -> format, &r, &g, &b);
-      if(r == 255 && g == 255 && b == 255)
+      SDL_GetRGB(pixel, img -> format, &r, &g, &b);    // recup les r g b du pixel (i, j) car vertical
+      if(r == 255 && g == 255 && b == 255)    // pareil qu'en version horizontal
       {white++;}
       else
       {black++;}
     }
   }
-  return(white / (black));
+  return(white) / (black );    //pareil qu'en horizontal
 }
-
-
-/*  RLSA algorithm  */
 
 void blockDetection_horizontal(SDL_Surface *img)
 { 
@@ -216,7 +214,7 @@ void blockDetection_horizontal(SDL_Surface *img)
   Uint8 r;
   Uint8 g;
   Uint8 b;
-  int hori = pixelSpacingHorizontal(img)*3;
+  int hori = pixelSpacingHorizontal(img)*30;    // c'est une moyenne de l'espacement, pour avoir un seuil adapté a l'image et pas reunir deux blocs entre eux par erreur
   int countWhite;
 
   for(int i = 0; i < img -> h; i++)
@@ -225,14 +223,14 @@ void blockDetection_horizontal(SDL_Surface *img)
     for(int j = 0; j < img -> w; j++)
     {
       pixel = getpixel(img, j, i);
-      SDL_GetRGB(pixel, img -> format, &r, &g, &b);
-      if(r == 255 && g == 255 && b == 255)
+      SDL_GetRGB(pixel, img -> format, &r, &g, &b);    // on recup le rgb de (j, i) car horitzontal
+      if(r == 255 && g == 255 && b == 255)    // on ajoute un blanc au compteur si le pixel est blanc
       {
         countWhite++;
       }
-      if(r == 0 && g == 0 && b == 0)
+      if(r == 0 && g == 0 && b == 0)    // sinon on compare le nombre de pixels blancs entre les deux pixels noirs avec hori
       {
-        if(countWhite <= hori)
+        if(countWhite <= hori)    // si c'est inferieur on rempli l'espace de noir pour creer le bloc
         {
             int k = j - 1;
             while(countWhite > 0)
@@ -243,7 +241,7 @@ void blockDetection_horizontal(SDL_Surface *img)
               k--;
             }
         }
-        else
+        else    // sinon on a deux blocs separes donc il ne faut pas remplir l'espace
         {
           countWhite = 0;
         }
@@ -251,13 +249,15 @@ void blockDetection_horizontal(SDL_Surface *img)
     }
   }
 }
+
+// remplis les espaces verticaux entre deux pixels. fonctionne pareil que la version horizontale
 void blockDetection_vertical(SDL_Surface *img)
 {
   Uint32 pixel;
   Uint8 r;
   Uint8 g;
   Uint8 b;
-  int verti = pixelSpacingVertical(img)*3;
+  int verti = pixelSpacingVertical(img) * 3;
   int countBlack;
 
   for(int i = 0; i < img -> w; i++)
@@ -287,6 +287,104 @@ void blockDetection_vertical(SDL_Surface *img)
         else
         {
           countBlack = 0;
+        }
+      }
+    }
+  }
+}
+
+
+// dessine les blocs en les noircissant entierement (fait un ET logique du RLSA horizontal et RLSA vertical)
+// img c'est l'image qui va contenir des blocs remplis de noir
+// imgHor a subi bloc detection horizontal
+// imgver a subi bloc detection vertical
+void drawBlocks (SDL_Surface *img, SDL_Surface *imgHor, SDL_Surface *imgVer)
+{
+    Uint32 pixelHor;
+    Uint32 pixelVer;
+    Uint32 pixel;
+    Uint8 r;
+    Uint8 g;
+    Uint8 b;
+    for(int i = 1; i + 1 < img -> w; i++)
+    {
+        for(int j = 1; j + 1< img -> h; j++)
+        {
+            pixelHor = getpixel(imgHor, i, j);
+            SDL_GetRGB(pixelHor, imgHor -> format, &r, &g, &b);
+            if (r == 0 && g == 0 && b == 0)
+            {
+                pixelVer = getpixel(imgVer, i, j);
+                SDL_GetRGB(pixelVer, imgVer -> format, &r, &g, &b);
+                if (r==0 && g==0 && b==0)
+                {
+                    pixel = SDL_MapRGB(img -> format, 0, 0, 0);
+                     putpixel(img, i, j, pixel);
+                }
+            }
+        }
+    }
+}
+
+
+// Trace les lignes des blocs avec le RLSA
+// img c'est l'image avec le texte et imgRLSA c'est l'image qui a subi blockDrawing
+void drawBlocksLines (SDL_Surface *img, SDL_Surface *imgRLSA)
+{
+  Uint32 pixel;
+  Uint32 pixelRLSA;
+  Uint32 pixelUp;
+  Uint32 pixelDown;
+  Uint32 pixelLeft;
+  Uint32 pixelRight;
+  Uint8 r;
+  Uint8 g;
+  Uint8 b;
+
+  for(int i = 1; i + 1 < img -> w; i++)
+  {
+    for(int j = 1; j + 1< img -> h; j++)
+    {
+      pixelRLSA = getpixel(imgRLSA, i, j);
+      SDL_GetRGB(pixelRLSA, imgRLSA -> format, &r, &g, &b);
+      if (r == 0 && g == 0 && b == 0)    // si le pixel est noir, on regarde les pixels autour, si un d'eux est blanc alors
+      {                                        // notre pixel est en bordure de bloc donc on le rend bleu
+        pixelUp = getpixel(imgRLSA, i, j-1);
+        SDL_GetRGB(pixelUp, imgRLSA -> format, &r, &g, &b);
+        if (r == 255 && g == 255 && b == 255)    // pixel du haut
+        {
+          pixel = SDL_MapRGB(img -> format, 0, 0, 255);
+          putpixel(img, i, j, pixel);
+        }
+        else
+        {
+          pixelDown = getpixel(imgRLSA, i, j+1);
+          SDL_GetRGB(pixelDown, imgRLSA -> format, &r, &g, &b);
+          if(r == 255 && g == 255 && b == 255)    // pixel d'en bas
+          {
+            pixel = SDL_MapRGB(img -> format, 0, 0, 255);
+            putpixel(img, i, j, pixel);
+          }
+          else
+          {
+            pixelLeft = getpixel(imgRLSA, i-1, j);
+            SDL_GetRGB(pixelLeft, imgRLSA -> format, &r, &g, &b);
+            if(r == 255 && g == 255 && b == 255)    // pixel de gauche
+            {
+              pixel = SDL_MapRGB(img -> format, 0, 0, 255);
+              putpixel(img, i, j, pixel);
+            }
+            else
+            {
+              pixelRight = getpixel(imgRLSA, i+1, j);
+              SDL_GetRGB(pixelRight, imgRLSA -> format, &r, &g, &b);
+              if(r == 255 && g == 255 && b == 255)    // pixel de droite
+              {
+                pixel = SDL_MapRGB(img -> format, 0, 0, 255);
+                putpixel(img, i, j, pixel);
+              }
+            }
+          }
         }
       }
     }
@@ -342,6 +440,7 @@ int seuil(SDL_Surface *img)
     for(int i = 0;i < img -> w;i++){
         if (histog[i]==0){
             s++;
+            //printf("S : %i \n",s);
         }
         else
         {
@@ -352,19 +451,54 @@ int seuil(SDL_Surface *img)
         }
         
     }
+    printf("MAX : %i \n",max);
     return max;
 }
 
+int average(SDL_Surface *img)
+{
+    int* histog=histo(img);
+    int s =0;
+    int sum =0;
+    int r = 0;
+    int bool1 =1;
+    for(int i = 0;i < img -> w;i++){
+        if (histog[i]==0){
+            if(bool1)
+            {
+                bool1 =0;
+                r+=1;
+            }
+            s++;
+        }
+        else
+        {
+            sum+=s;
+            bool1=1;
+            s =0;
+        }
+        
+    }
+    printf("AVERAGE : %i ", sum/r);
+    return sum/r;
+    
+}
+
 void cutword(SDL_Surface *img){
+
+    printf("W %i H %i",img -> w, img ->h);
   int* histog=histo(img);
   int seuil1 = seuil(img);
   int bool1 = 1;
+  int r = average(img);
   int s =0;
   int pos = 0;
-  printf("%i",seuil1);
+  int i;
+  
+  printf("SEUIL : %i\n",seuil1);
  
-  for (int i = 0; i<img -> w; i++){
-      printf("%i \n",s);
+  for (i = 0; i<img -> w; i++){
+      //printf("%i \n",s);
    if (histog[i]==0){
        if(bool1){
            pos = i;
@@ -373,13 +507,18 @@ void cutword(SDL_Surface *img){
         s++;
     }
     else{
-        if (s>= seuil1 -5 && s <= seuil1 +5)
+        //printf("S : %i",s);
+        if (s>= seuil1 - 1.5*r)
         {
             DrawAColumn(img, pos, 0,img->h);
         }
         bool1=1;
         s =0;
     }
-    
   } 
+
+  if (i == img -> w)
+  {
+      DrawAColumn(img, pos, 0,img->h);
+  }
 }
